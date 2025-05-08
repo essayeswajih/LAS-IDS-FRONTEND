e755156d91445f78560b2dffe1220ddc20fd3e13
essayeswajih <wajihsayes@gmail.com>
2025-05-08 11:18:13 +0100

    5/8/25

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   production: true,
   stripePublicKey: 'pk_test_51R85AjQx0D7b1Bx53wn5U5CICy3ze6V3TPDUIa0k80hBIAjyPZ65PgFq1DDB7S4Q15gTdFn1Sze9U6d1YLDlpHeO00F6woqfPS',
   baseUrl: "https://las-ids-backend-1.onrender.com",
-  wsUrl :"ws://las-ids-backend-1.onrender.com"
+  wsUrl :"wss://las-ids-backend-1.onrender.com"
 };
